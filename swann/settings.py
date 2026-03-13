2a9ebf3d56f8e65daabe3baf0f660e3f7009818f
swann-developer <swann.developer@gmail.com>
2026-03-14 04:26:45 +0530

    add production database dump

--- swann/settings.py
+++ swann/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = 'swann.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.getenv("DATABASE_URL")
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
 DATABASES = {
