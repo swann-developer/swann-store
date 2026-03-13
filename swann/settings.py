b29cdaaa09ef48d7e723f431ee376f56d9338b77
swann-developer <swann.developer@gmail.com>
2026-03-14 02:00:11 +0530

    postgres

--- swann/settings.py
+++ swann/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'swann.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.getenv("DATABASE_URL")
+#     )
+# }
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
     )
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
