9e7324a49771e9b56756f5da2bad3cfc9bd07e6c
swann-developer <swann.developer@gmail.com>
2026-03-15 22:48:31 +0530

    domain connect

--- swann/settings.py
+++ swann/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "swann-store.onrender.com",
+    "swannbyswapna.com",
+    "www.swannbyswapna.com",
+]
 
 
 # Application definition
