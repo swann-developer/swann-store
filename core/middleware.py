3be2999ad69508641ac416417e8e938700454371
swann-developer <swann.developer@gmail.com>
2026-03-17 00:56:22 +0530

    middleware fix

--- core/middleware.py
+++ core/middleware.py
@@ -1,27 +1,27 @@
 from django.http import Http404
 from django.conf import settings
+from django.http import HttpResponseForbidden
 
 class HideAdminMiddleware:
-    admin_path = f"/{settings.ADMIN_URL}"
-    # allow admin login page
-    if request.path.startswith(admin_path):
-        if request.path == admin_path + "login/":
-            return self.get_response(request)
-        if not request.user.is_authenticated:
-            raise Http404()
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path.startswith(f"/{settings.ADMIN_URL}"):
+
+        admin_path = f"/{settings.ADMIN_URL}"
+
+        if request.path.startswith(admin_path):
+
+            # allow login page
+            if request.path == admin_path + "login/":
+                return self.get_response(request)
+
             if not request.user.is_authenticated:
                 raise Http404()
 
         return self.get_response(request)
 
-
-from django.http import HttpResponseForbidden
 
 class BlockAdminBotsMiddleware:
 
