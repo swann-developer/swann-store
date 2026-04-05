44a88202ab98f368b2aed6f4e9a17037b27f8662
swann-developer <swann.developer@gmail.com>
2026-04-05 22:30:56 +0530

    Cloudinary for images fix-prod-thumbnail-bugfix

--- swann/urls.py
+++ swann/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+
+
 urlpatterns = [
+    # path("admin/", admin.site.urls),
     path(settings.ADMIN_URL, admin.site.urls),
     path("", include("core.urls")),
 ]
